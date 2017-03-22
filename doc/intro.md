48af4a5f2f969b0cbdfc5a88de3690b6b3fb6b7b
Rafik Naccache <rafik.naccache@gmail.com>
2017-03-22 22:09:23 +0100

    First commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to tasks
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/tasks/core_test.clj
@@ -0,0 +1,7 @@
+(ns tasks.core-test
+  (:require [clojure.test :refer :all]
+            [tasks.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
