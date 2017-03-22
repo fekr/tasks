3e7c7c43f9fa0cecaa662190db35c4ae02f64575
Rafik Naccache <rafik.naccache@gmail.com>
2017-03-22 23:20:11 +0100

    Bumped milestones version

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :min-lein-version "2.7.1"
   
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojars.turbopape/milestones "1.0.0"]
+                 [org.clojars.turbopape/milestones "1.0.1"]
                  [postagga "0.2.4"]
                  [org.clojure/clojurescript "1.9.229"]]
 
